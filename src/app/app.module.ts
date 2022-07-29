12a8cb5198e327328a5c27004e489b97c9e9cfd2
ummi <ummiusac@gmail.com>
2022-07-29 13:04:44 -0600

    adding automatic invatitions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -8,7 +9,13 @@ import { ConfirmComponent } from "./confirm/confirm.component";
 
 @NgModule({
   declarations: [AppComponent, ConfirmComponent],
-  imports: [BrowserModule, NgbModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
