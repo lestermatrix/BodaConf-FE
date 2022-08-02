ca01002a3f9516112d7e4d947e15b984eb8b96b4
ummi <ummiusac@gmail.com>
2022-08-01 22:39:47 -0600

    adding text and imgs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from "@angular/common/http";
 import { ConfirmComponent } from "./confirm/confirm.component";
 import { WeddingConfirmComponent } from "./wedding-confirm/wedding-confirm.component";
 
-const routes: Routes = [{ path: "confirm/:id", component: ConfirmComponent }, { path: "wedding-confirm/:id", component: WeddingConfirmComponent }];
+const routes: Routes = [
+  { path: "confirm/:id", component: WeddingConfirmComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), HttpClientModule],
   exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
